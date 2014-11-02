476d6ec3af07c99472fe60a30edbc7f961812c1f
Pattarawat Chormai <goonnow@rocketmail.com>
2014-11-02 15:23:14 +0700

    Correct s3 url.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,8 @@ Rails.application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region => 'ap-southeast-1'
     }
+    :path => ":class/:id/:basename_:style.:extension",
+    :url  => ":s3_sg_url"
   }
 end
